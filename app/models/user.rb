66c663ce2c433aabf1d56628b51c3a761695010e
kousei-ya <k.yano@genbasupport.com>
2024-11-05 17:13:52 +0900

    ファイルタイプを制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :user_icon
+
+  private
+
+  def verify_file_type
+    return unless user_icon.attached?
+
+    allowed_file_types = %w[image/jpg image/gif image/png]
+    errors.add(:user_icon, 'only jpg, jpeg, gif, png') unless allowed_file_types.include?(user_icon.blob.content_type)
+  end
 end
