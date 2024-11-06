0c074639bef5ed9c08203a8c1920b05fcba473b4
kousei-ya <k.yano@genbasupport.com>
2024-11-06 10:28:50 +0900

    不要な部分を削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'books#index'
   resources :books
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show]
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
