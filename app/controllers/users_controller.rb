50251cb4a7ac32a56517809aa47e3405c0d90460
kousei-ya <k.yano@genbasupport.com>
2024-11-06 10:27:51 +0900

    重複しているメソッドを削除しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user! # ログイン必須
 
   def index
     @users = User.page(params[:page]).per(5).order(:id)
@@ -9,14 +8,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user  # ここでリダイレクト
-    else
-      render :edit
-    end
   end
 end
