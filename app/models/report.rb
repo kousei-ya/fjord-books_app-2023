e2628fd006f689a956cd961c62e05da929e32d74
kousei-ya <k.yano@genbasupport.com>
2024-11-14 15:46:16 +0900

    言及先と言及元の日報の関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,13 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :report_mentions, foreign_key: :report_id, dependent: :destroy
+  has_many :mentioning_reports, through: :report_mentions, source: :mentioned_report
+  has_many :reverse_report_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :mentioned_reports, through: :reverse_report_mentions, source: :report
+
+  after_save :update_mentions
+  before_destroy :remove_mentions
 
   validates :title, presence: true
   validates :content, presence: true
