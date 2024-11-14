6ddddf5a703c8851dff3e43031fd9ab514fdd826
kousei-ya <k.yano@genbasupport.com>
2024-11-14 16:09:39 +0900

    rubocopを通しました

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,9 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :report_mentions, foreign_key: :report_id, dependent: :destroy
+  has_many :report_mentions, dependent: :destroy
   has_many :mentioning_reports, through: :report_mentions, source: :mentioned_report
-  has_many :reverse_report_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :reverse_report_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioned_reports, through: :reverse_report_mentions, source: :report
 
   after_save :update_mentions
@@ -23,18 +23,18 @@ class Report < ApplicationRecord
   end
 
   def update_mentions
-    mentioned_report_ids = extract_mentioned_report_ids(self.content)
-    self.report_mentions.where.not(mentioned_report_id: mentioned_report_ids).destroy_all
+    mentioned_report_ids = extract_mentioned_report_ids(content)
+    report_mentions.where.not(mentioned_report_id: mentioned_report_ids).destroy_all
     mentioned_report_ids.each do |mentioned_report_id|
-      self.report_mentions.find_or_create_by(mentioned_report_id: mentioned_report_id)
+      report_mentions.find_or_create_by(mentioned_report_id:)
     end
   end
 
   def remove_mentions
-    self.report_mentions.destroy_all
+    report_mentions.destroy_all
   end
 
   def extract_mentioned_report_ids(text)
     text.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
   end
-end  
+end
--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReportMention < ApplicationRecord
   belongs_to :report
   belongs_to :mentioned_report, class_name: 'Report'
