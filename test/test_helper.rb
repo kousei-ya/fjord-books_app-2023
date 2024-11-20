43e749f9924dc1d75bac64cb9a203e95801bb801
kousei-ya <k.yano@genbasupport.com>
2024-11-20 11:57:29 +0900

    test_helper.rnを編集しました

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'application_system_test_case'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
