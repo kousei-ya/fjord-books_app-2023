0f1af6df714f75c9b63ceb73af5fa1350405e7bb
kousei-ya <k.yano@genbasupport.com>
2024-11-25 10:11:29 +0900

    reportテストでfixtureファイルを使うようにしました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,30 +4,30 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '日報を編集できればeditable?がtrueを返す' do
-    kousei = User.create!(email: 'kousei@example.com', password: 'password')
+    kousei = users(:kousei)
     report = kousei.reports.create!(title: '今日も頑張った', content: '帰りましょう')
 
     assert report.editable?(kousei)
   end
 
   test '日報を編集できなければeditable?がfalseを返す' do
-    bob = User.create!(email: 'Bob@example.com', password: 'password')
-    carol = User.create!(email: 'Carol@example.com', password: 'password')
+    bob = users(:bob)
+    carol = users(:carol)
     report = bob.reports.create!(title: '今日も頑張った', content: '帰りましょう')
 
     assert_not report.editable?(carol)
   end
 
   test 'レポートを作成した日付を取得する' do
-    dave = User.create!(email: 'Dave@example.com', password: 'password')
+    dave = users(:dave)
     report = dave.reports.create!(title: '今日も頑張った', content: '帰りましょう', created_at: Time.zone.local(2024, 11, 18, 15, 30, 0))
 
     assert_equal Date.new(2024, 11, 18), report.created_on
   end
 
   test '日報に他の日報のURLが含まれている時に対象の日報をmentioning_reportsに追加する' do
-    eve = User.create!(email: 'Eve@example.com', password: 'password')
-    frank = User.create!(email: 'Frank@example.com', password: 'password')
+    eve = users(:eve)
+    frank = users(:frank)
 
     report1 = eve.reports.create!(id: 1, title: 'report1', content: 'http://localhost:3000/reports/2')
     report2 = frank.reports.create!(id: 2, title: 'report2', content: 'http://localhost:3000/reports/3')
