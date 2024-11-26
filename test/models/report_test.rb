e89a3501e322d1fa2c5b386866a9bf47b747e445
kousei-ya <k.yano@genbasupport.com>
2024-11-26 09:31:42 +0900

    テストで使用する人数を減らし、ymlの量を減らしました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -11,27 +11,27 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '日報を編集できなければeditable?がfalseを返す' do
+    kousei = users(:kousei)
     bob = users(:bob)
-    carol = users(:carol)
     report = bob.reports.create!(title: '今日も頑張った', content: '帰りましょう')
 
-    assert_not report.editable?(carol)
+    assert_not report.editable?(kousei)
   end
 
   test 'レポートを作成した日付を取得する' do
-    dave = users(:dave)
-    report = dave.reports.create!(title: '今日も頑張った', content: '帰りましょう', created_at: Time.zone.local(2024, 11, 18, 15, 30, 0))
+    kousei = users(:kousei)
+    report = kousei.reports.create!(title: '今日も頑張った', content: '帰りましょう', created_at: Time.zone.local(2024, 11, 18, 15, 30, 0))
 
     assert_equal Date.new(2024, 11, 18), report.created_on
   end
 
   test '日報に他の日報のURLが含まれている時に対象の日報をmentioning_reportsに追加する' do
-    eve = users(:eve)
-    frank = users(:frank)
+    kousei = users(:kousei)
+    bob = users(:bob)
 
-    report1 = eve.reports.create!(title: 'report1', content: "いい天気ですね")
-    report2 = frank.reports.create!(title: 'report2', content: "http://localhost:3000/reports/#{report1.id}")
-    report3 = frank.reports.create!(title: 'report3', content: "http://localhost:3000/reports/#{report2.id}")
+    report1 = kousei.reports.create!(title: 'report1', content: "いい天気ですね")
+    report2 = bob.reports.create!(title: 'report2', content: "http://localhost:3000/reports/#{report1.id}")
+    report3 = bob.reports.create!(title: 'report3', content: "http://localhost:3000/reports/#{report2.id}")
 
     assert_includes report2.mentioning_reports, report1
     assert_includes report3.mentioning_reports, report2
